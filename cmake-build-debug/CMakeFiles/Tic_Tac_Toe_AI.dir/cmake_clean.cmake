abe29c1c2176153a5e8a2b330f1d4e3ba497592a
nickyichov <nikiichov99@gmail.com>
2021-11-23 12:43:46 +0100

    main and CMake list uploaded

---
+++ cmake-build-debug/CMakeFiles/Tic_Tac_Toe_AI.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tic_Tac_Toe_AI.dir/main.cpp.obj"
+  "Tic_Tac_Toe_AI.exe"
+  "Tic_Tac_Toe_AI.exe.manifest"
+  "Tic_Tac_Toe_AI.pdb"
+  "libTic_Tac_Toe_AI.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tic_Tac_Toe_AI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
