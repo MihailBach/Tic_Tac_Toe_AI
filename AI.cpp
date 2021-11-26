b1ed91ecbfc19031c4aed056c16e125180e214c0
nickyichov <nikiichov99@gmail.com>
2021-11-26 13:53:58 +0100

    AI .ccp and .h committed

---
+++ AI.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nicky Ichov on 26/11/2021.
+//
+
+#include "AI.h"
---
+++ AI.h
@@ -0,0 +1,14 @@
+//
+// Created by Nicky Ichov on 26/11/2021.
+//
+
+#ifndef TIC_TAC_TOE_AI2_AI_H
+#define TIC_TAC_TOE_AI2_AI_H
+
+
+class AI {
+
+};
+
+
+#endif //TIC_TAC_TOE_AI2_AI_H
